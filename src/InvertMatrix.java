ee1c2b0464835b502ffccbf6a3080594242ad05f
melisapo <129457523+melisapo@users.noreply.github.com>
2024-04-23 22:27:13 -0400

    primer commit :P

---
+++ src/InvertMatrix.java
@@ -0,0 +1,25 @@
+public class InvertMatrix {
+    public static void main(String[] args) throws Exception {
+        //Maikol
+        //Leer la matriz, llamar el metodo, mostrar el resultado
+        int [][] matriz = new int[3][3];
+    }
+    public static double[][] Inversa(int[][]matriz){
+        //Dividir cada elemento de la matriz adjunta por el determinante
+    }
+    public static int[][] Adjunta(int[][]matriz){
+        //Yordanis
+        //La traspuesta de los cofactores
+    }
+    public static int Cofactor(int i, int j){
+        //Nilo
+        // Cij=1^(i+j)*Mayor
+    }
+    public static int Mayor(int[][]matriz, int i,int j){
+        //Melissa
+    }
+    public static int Determinante(int[][]matriz){
+        //Kevin
+        //El determinante de la matriz 3x3
+    }
+}
